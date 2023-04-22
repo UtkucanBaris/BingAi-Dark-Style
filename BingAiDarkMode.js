aa5f6e6bbd321fde38ef56e2ed513ff222d86fca
UtkucanBaris <utkuduha@hotmail.com>
2023-04-22 20:49:57 +0300

    Update BingAiDarkMode.js

--- BingAiDarkMode.js
+++ BingAiDarkMode.js
@@ -3,10 +3,11 @@
 // Author: Utkucan
 // Date: 19/04/2023
 // This Code Works At this Date
-document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)").remove();
-document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)").remove();
+if(document.querySelector("#b_sydConvCont > cib-serp") != null && document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)") != null){document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)").remove();}
+if(document.querySelector("#b_sydConvCont > cib-serp")!=null && document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)") != null){document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)").remove();}
+// document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)").remove();
+// document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").shadowRoot.querySelector("div:nth-child(1)").remove();
 document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-background").style.backgroundColor="black";
-
 // document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller.scroller-enabled > div.fade.top > cib-background").shadowRoot.querySelector("div:nth-child(1)").setAttribute("style","background-position: 50% 0%;background: black;")
 // document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller.scroller-enabled > div.fade.top > cib-background").shadowRoot.querySelector("div:nth-child(2)").setAttribute("style","background-position: 50% 0%;background: black;")
 // document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller.scroller-enabled > div.fade.bottom > cib-background").shadowRoot.querySelector("div:nth-child(1)").setAttribute("style","background-position: 50% 0%;background: black;")
@@ -15,10 +16,10 @@ document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("ci
 // document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller > div.fade.top").remove()
 // document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller > div.fade.bottom").remove()
 
-document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller.scroller-enabled > div.fade.top > cib-background").shadowRoot.querySelectorAll(".image").forEach(e => e.remove());
-document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller.scroller-enabled > div.fade.top > cib-background").setAttribute("style","background: black !important;background-position: 50% 0%;");
-document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller.scroller-enabled > div.fade.bottom > cib-background").shadowRoot.querySelectorAll(".image").forEach(e => e.remove());
-document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller.scroller-enabled > div.fade.bottom > cib-background").setAttribute("style","background: black !important;background-position: 50% 0%;");
+document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller > div.fade.top > cib-background").shadowRoot.querySelectorAll(".image").forEach(e => e.remove());
+document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller > div.fade.top > cib-background").setAttribute("style","background: black !important;background-position: 50% 0%;");
+document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller > div.fade.bottom > cib-background").shadowRoot.querySelectorAll(".image").forEach(e => e.remove());
+document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("div.scroller > div.fade.bottom > cib-background").setAttribute("style","background: black !important;background-position: 50% 0%;");
 
 var styleElement = document.createElement("style");
 styleElement.type = "text/css";
@@ -28,6 +29,5 @@ document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("ci
 document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("cib-serp-feedback").shadowRoot.querySelector("#fbpgbt > svg-icon").style.fill = "rgb(145 0 0)"
 document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("#searchbox").style.color="white";
 document.querySelector("#b_sydConvCont > cib-serp").setAttribute("style","--cib-color-neutral-foreground: white;--cib-color-brand-link-foreground: red;--cib-color-brand-primary-background: linear-gradient(90deg, #000000 10.79%, #d90707b8 87.08%);--cib-color-neutral-layer-card: black;--cib-color-brand-gradient-core: linear-gradient(90deg, #000000 10.79%, #810000 87.08%);--cib-color-brand-tertiary-foreground: white;--cib-color-brand-tertiary-background: #7c0000;--cib-color-stealth-primary-foreground: white;--cib-color-brand-text-highlight-background: black;--cib-color-stealth-primary-foreground-hover: red;--cib-color-brand-secondary-stroke: red;--cib-color-brand-secondary-background: black;--cib-color-brand-secondary-foreground: white;--cib-color-brand-secondary-background-solid: black;--cib-color-neutral-input-inverted:black;--cib-color-neutral-layer-overlay: black;--cib-color-brand-secondary");
-document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("div > div.main-container").setAttribute("style","background:black;outline:1px solid red")// This code is hosted on Github at https://github.com/UtkucanBaris/BingAi-Dark-Style.
-// You can find more information and updates there. If you have any feedback or suggestions, please leave a comment or open an issue.
+document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("div > div.main-container").setAttribute("style","background:black;outline:1px solid red")// You can find more information and updates there. If you have any feedback or suggestions, please leave a comment or open an issue.
 // Thank you for using this code and have a nice day!
